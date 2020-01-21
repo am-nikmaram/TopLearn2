4a2c8579099caa3d30e28c91da8b80f2c6807389
amir nikmaram <am.nikmaram@hotmail.com>
2020-01-21 14:03:02 +0330

    search page

--- TopLearn.Web/Controllers/CourseController.cs
+++ TopLearn.Web/Controllers/CourseController.cs
@@ -17,6 +17,7 @@ namespace TopLearn.Web.Controllers
         }
         public IActionResult Index(int pageId = 1, string filterTitle = "", string getType = "all", string sortByType = "Data", int startPrice = 0, int endPrice = 0, List<int> selectedGroups = null)
         {
+            ViewBag.courseType = getType;
             ViewBag.sortType = sortByType;
             ViewBag.filter = filterTitle;
             ViewBag.selectedgroups = selectedGroups;
